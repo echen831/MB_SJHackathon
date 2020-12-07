b81c8bd68e6c585cbf02c435b1a6b32323e2931d
your name here <echen831@gmail.com>
2020-12-06 17:09:35 -0800

    using covid data api

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Map = ({ names }) => {
 
-    return (
-        <div className='map-container'>
-            Map goes here...
-            <ul>
-                {names.map((name, idx) => {
-                    return(
-                        <div key={idx}>
-                            <li>{name.name}</li>
-                            <li>{name.address}</li>
-                        </div>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.covidtracking.com/v1/states/info.json')
+            .then(res => res.json())
+            .then((result) => {
+                setIsLoaded(true);
+                setItems(result);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error)
+            }
+        )
+    }, [])
+
+    if (error) {
+        return (
+            <div>Error: {error.message}</div>
+        )
+    } else if (!isLoaded) {
+        return <div>Loading...</div>
+    } else {
+        return (
+            <div>
+                <ul>
+                    {items.map(state => {
+                        return <li>{state.state}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+    // return (
+    //     <div className='map-container'>
+    //         {/* Map goes here...
+    //         <ul>
+    //             {names.map((name, idx) => {
+    //                 return(
+    //                     <div key={idx}>
+    //                         <li>{name.name}</li>
+    //                         <li>{name.address}</li>
+    //                     </div>
+    //                 )
+    //             })}
+    //         </ul> */}
+    //     </div>
+    // )
 }

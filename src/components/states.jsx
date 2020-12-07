423099c89cc9da5d706a89f9ceec46fdac913ee1
your name here <echen831@gmail.com>
2020-12-06 22:40:28 -0800

    add colors

--- src/components/states.jsx
+++ src/components/states.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import { STATES } from './states'
 
+const COLORS = ['red', 'blue', 'green', 'orange', 'purple', 'gold', 'indigo']
+
 export const States = ({ setCurrState }) => {
 
 
@@ -8,8 +10,9 @@ export const States = ({ setCurrState }) => {
     return (
         <div>
             <ul className='wrapper'>
-                {STATES.map(state => (
+                {STATES.map((state, idx) => (
                     <li key={state.abbreviation}
+                        style={{color: 'white', backgroundColor: COLORS[idx % COLORS.length]}}
                         className='button'
                         onClick={() => setCurrState(state.abbreviation)}>
                         {state.abbreviation}</li>

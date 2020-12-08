3a56979f74e1c9e5a36996b74e8cb6f5871c1f38
your name here <echen831@gmail.com>
2020-12-07 22:26:08 -0800

    add recharts for chart visual

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+
 
 export const History = ({ data, isLoaded, error }) => {
 
@@ -9,18 +14,23 @@ export const History = ({ data, isLoaded, error }) => {
     } else {
         return (
             <div>
-                Historic data...
-                <ul>
-                    {data.map(day => {
-                        return (
-                            <div>
-                                <li>Date: {day.date}</li>
-                                <li>Num Positive: {day.positive}</li>
-                            </div>              
-                        )
-                    })}
-    
-                </ul>
+                <h1>Historic Data</h1>         
+                <BarChart
+                    width={1200}
+                    height={500}
+                    data={data}
+                    margin={{
+                        top: 20, right: 30, left: 20, bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="positive" stackId="a" fill="#8884d8" />
+                    <Bar dataKey="negative" stackId="a" fill="#82ca9d" />
+                </BarChart>
             </div>
         )
 

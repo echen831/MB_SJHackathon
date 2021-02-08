d877404303edf5031df8aa2094807a4b420e5e6e
your name here <echen831@gmail.com>
2021-02-07 19:50:50 -0800

    update navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,9 +6,9 @@ export const Header = () => {
         // <div className='header'>
         //     <h1>Covid Daily Data</h1>
         // </div>
-        <nav>
-            <h1>Covid Daily Data</h1>
-            <ul>
+        <nav className='navbar'>
+            <h1 className='navbar-title'>Covid Daily Data</h1>
+            <ul className='navbar-list'>
                 <li><a href="#">Curriculum</a></li>
                 <li><a href="#">Pricing</a></li>
                 <li><a class="button" href="#">Contact</a></li>

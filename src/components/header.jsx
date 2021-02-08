438ca5f807004486f12136de27f0d130ae46c742
your name here <echen831@gmail.com>
2021-02-07 19:44:52 -0800

    update navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 export const Header = () => {
 
     return (
-        <div className='header'>
+        // <div className='header'>
+        //     <h1>Covid Daily Data</h1>
+        // </div>
+        <nav>
             <h1>Covid Daily Data</h1>
-        </div>
+            <ul>
+                <li><a href="#">Curriculum</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a class="button" href="#">Contact</a></li>
+            </ul>
+        </nav>
     )
 }
